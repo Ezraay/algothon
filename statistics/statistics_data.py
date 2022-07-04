d4447c78bca0ef5f96f4379f8914d6d2fddb5aac
LilCBZ <77474531+LilCBZ@users.noreply.github.com>
2022-07-05 00:32:06 +1000

    Added function to find resistances of stock. Also added resistance plotting.

--- statistics/statistics_data.py
+++ statistics/statistics_data.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 prices_data = pd.read_csv("prices.txt", delim_whitespace=True, header=None)
 num_stocks = len(prices_data.columns)
@@ -8,7 +7,8 @@ num_days = len(prices_data[0])
 
 variances = [np.var(prices_data[i]) for i in range(num_stocks)]
 
-def get_variance(stock_num: int):
+# Returns variance of specified stock.
+def variance(stock_num: int):
     return variances[stock_num]
 
 # Returns moving avg of size n at specified day.
@@ -23,6 +23,45 @@ def moving_average(stock_num: int, n: int, day_num: int):
 # Returns a list of the moving avg of size n at every day up to end_day.
 def complete_moving_avg(stock_num: int, n: int,  end_day: int = num_days):
     return [moving_average(stock_num, n, i) for i in range(n - 1, end_day)]
+
+# Returns list of "active" resistances. An active resistance is a resistance
+# that has not been violated. List is implicitly ascending. At most one resistance
+# will be retrieved per interval.
+def active_resistances(stock_num: int, interval_size: int, remove_last: bool = True):
+    stock_price = list(prices_data[stock_num]) # Stock price on each day.
+    values = [stock_price[0]] # List of the resistances.
+    num_intervals = num_days // interval_size
+    if num_days % interval_size != 0:
+        num_intervals += 1
+
+    # Builds values.
+    for i in range(num_intervals):
+        # Finds minimum stock value in interval.
+        day = i * interval_size
+        min = stock_price[day]
+        for j in range(interval_size):
+            day = i * interval_size + j
+
+            if(day >= num_days):
+                break
+            
+            if stock_price[day] < min:
+                min = stock_price[day]
+
+        # Removes violated resistances.
+        while len(values) > 0 and values[-1] > min:
+            values.pop()
+        
+        values.append(min)
+    
+    # Remove last probably desired since last resistance is often just the final stock price.
+    if remove_last:
+        values.pop()
+
+    return values
+
+
+
 
 
 
--- statistics/statistics_visuals.py
+++ statistics/statistics_visuals.py
@@ -11,12 +11,14 @@ def plot_stock_moving_avg(stock_num: int, n: int,  end_day: int = sd.num_days):
     x_axis = [i + n - 1 for i in range(len(sd.complete_moving_avg(stock_num, n)))] # Handles translation of x axis.
     plt.plot(x_axis, sd.complete_moving_avg(stock_num, n, sd.num_days), label = "avg " + str(n))
 
+# Plots horizontal lines representing the active resistances for the stock and specified interval.
+def plot_active_resistances(stock_num: int, interval: int):
+    for i in sd.active_resistances(stock_num, interval):
+        plt.axhline(i)
 
-plot_stock(4)
-plot_stock_moving_avg(4, 25)
-plot_stock_moving_avg(4, 60)
-plot_stock_moving_avg(4, 120)
 
+plot_stock(9)
+plot_active_resistances(9, 30)
 
 plt.legend(loc = 9)
 
